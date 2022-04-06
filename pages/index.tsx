dd3ddf38093755677607f8a8add383c4d76cd074
cxii112 <lyakhovoy.d@ya.ru>
2022-04-06 16:25:41 +0500

    default layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,14 +4,13 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Nav from '../components/nav'
 import React from 'react'
+import DefaultLayout from "../components/DefaultLayout";
 
 export default function Home() {
   return(
     <>
-      <Nav label="Расписание"/>
-      <div className="wrapper w-full h-[10000px]">
-
-      </div>
+      <DefaultLayout label={"Расписание"}>
+      </DefaultLayout>
     </>
   )
 }
